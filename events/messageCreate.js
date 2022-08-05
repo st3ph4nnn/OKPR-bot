2e5a335419eae7e7a5c30dcfbb716627c973ec53
stefan <stephanarteni@gmail.com>
2022-08-05 21:14:56 +0300

    markov fix

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -90,6 +90,7 @@ module.exports = {
 						message.content = message.content.replace(/\r?\n|\r/g, " ");
 
 						fs.appendFileSync('database/strings.txt', message.content + ' ');
+						
 						try {
     						await client.ftp.uploadFrom('database/strings.txt', 'strings.txt');
         				} catch {
@@ -106,7 +107,6 @@ module.exports = {
         					return;
         				}
 
-						await client.ftp.downloadTo("database/strings.txt", "strings.txt");
 						let s = fs.createReadStream('database/strings.txt');
 
 						try {
