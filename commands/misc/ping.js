a069c81dd5caea513667021227619dfd3971c8dd
stefan <stephanarteni@gmail.com>
2022-08-05 15:27:23 +0300

    owner id changes, fixed image bug

--- commands/misc/ping.js
+++ commands/misc/ping.js
@@ -7,7 +7,15 @@ module.exports = {
 	category: 'Misc',
 	cooldown: 1000,
 	async execute(message, args, client) {
-		const ping_embed = new embed(message, 'Ping Pong <:troll_romania:996060026093441104>', `API Latency: \`${Math.round(client.ws.ping)}ms\`\nUptime: ${moment.duration(client.uptime).format(" D [days], H [hrs], m [mins], s [secs]")}`);
+
+		let totalSeconds = (bot.uptime / 1000);
+		let days = Math.floor(totalSeconds / 86400);
+		let hours = Math.floor(totalSeconds / 3600);
+		totalSeconds %= 3600;
+		let minutes = Math.floor(totalSeconds / 60);
+		let seconds = totalSeconds % 60;
+
+		const ping_embed = new embed(message, 'Ping Pong <:troll_romania:996060026093441104>', `API Latency: \`${Math.round(client.ws.ping)}ms\`\nUptime: ${days} day(s), ${hours} hour(s), ${minutes} minute(s), ${seconds} second(s)`);
 		ping_embed.send();
 	}
 }
