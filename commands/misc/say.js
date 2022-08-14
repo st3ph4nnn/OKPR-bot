e75a668d3e07148ec9563228b6f3ac64b0c7fa8c
stephan <stephan@localhost.localdomain>
2022-08-14 03:54:50 +0300

    update masiv, prea multe lucruri de spus

--- commands/misc/say.js
+++ commands/misc/say.js
@@ -3,9 +3,12 @@ module.exports = {
 	aliases: ['spune'],
 	description: 'Fă botul să spună ceva.',
 	category: 'Misc',
+	permissions: [ 'ADMINISTRATOR' ]
 	cooldown: 10000,
 	async execute(message, args, client) {
 		if (!args[0]) return message.reply('Ce vrei să spun?');
+
+		if (message.mentions.members.first()) return message.reply('Nu permit mentiuni!!!');
 
 		await message.delete({timeout: 1000});
 
