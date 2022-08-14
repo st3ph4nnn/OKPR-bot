63de67751338da0a7211ff84b77c531b1ffed2b6
stephan <stephan@localhost.localdomain>
2022-08-14 04:02:39 +0300

    update masiv, prea multe lucruri de spus

--- commands/misc/say.js
+++ commands/misc/say.js
@@ -7,7 +7,7 @@ module.exports = {
 	async execute(message, args, client) {
 		if (!args[0]) return message.reply('Ce vrei să spun?');
 
-		if (message.mentions.members.first() || message.mentions.everyone) return message.reply('Nu permit mentiuni!!!');
+		if (message.mentions.members.first() || message.mentions.everyone) return message.reply('Nu permit mențiuni :nerd:!!!');
 
 		await message.delete({timeout: 1000});
 
